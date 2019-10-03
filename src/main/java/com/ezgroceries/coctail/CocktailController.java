1d9a609e527b1bdbe63be3e243fcb06e74f06542
Elke Donny <elke.donny@kbc.be>
2019-10-03 13:12:28 +0200

    Lab 2

--- src/main/java/com/ezgroceries/coctail/CocktailController.java
+++ src/main/java/com/ezgroceries/coctail/CocktailController.java
@@ -1,10 +1,13 @@
 package com.ezgroceries.coctail;
 
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
